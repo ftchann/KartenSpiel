4e4f3c94445c837fd6fa820634aae9360d0a8896
shivus123 <philipp.niederlenz2@gmail.com>
2020-05-26 14:59:34 +0200

    button

--- App.js
+++ App.js
@@ -1,73 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Text, View, ScrollView, Image, Button } from 'react-native';
 
-const App: () => React$Node = () => {
+export default function App() {
   return (
-    <>
-      <StatusBar />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Text>sdgbfibasdiufbiuasd</Text>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <ScrollView>
+    <View style={{
+      flex: 1, 
+      justifyContent: "center", 
+      alignItems: "center"
+      }}>
+      <Text>
+        Try editing me! 🎉
+      </Text>
+      <Button onPress={() => {
+        alert('You tapped the buttonè!');
+        }}
+        title="Play"
+      />
+    </View>
+    </ScrollView>
   );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+}
