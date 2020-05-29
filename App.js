10fd44c9edbbf6269f39b4425825b5cd7a5fe2c2
shivus123 <philipp.niederlenz2@gmail.com>
2020-05-29 10:25:47 +0200

    stack navigation

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
-import React, { useState } from 'react';
-import { Text, View, Button, ScrollView} from 'react-native';
+// In App.js in a new project
 
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-    const [isVisible, setIsVisible] = useState(false);
+function HomeScreen( {navigation} ) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+            title="Go to Game"
+            onPress={()=> {navigation.navigate('Game')}}
+        />
+    
+    </View>
+  );
+}
+
+function GameScreen( {navigation} ) {
     return (
-        <View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Game Screen</Text>
             <Button
-                onPress={() => {
-                    setIsVisible(true);
-                }}
-                title="Activate"
+                title="Go to Home"
+                onPress={()=> {navigation.navigate('Home')}}
             />
-            <Button
-                onPress={() => {
-                    setIsVisible(false);
-                }}
-                title="Deactivate"
-            />
-            {isVisible?<Text> get printed </Text>:null}
-            <Text>{isVisible ? "hello": null}</Text>
         </View>
     );
 }
 
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Game" component={GameScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
